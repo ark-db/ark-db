ec239cbf2c0f7a74aaef63a7ad133581f343e480
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-25 12:15:49 -0400

    add error screen

---
+++ src/lib/components/ErrorScreen.svelte
@@ -0,0 +1,24 @@
+<script>
+    import skalter from "./skalter.jpg"
+    export let status;
+    export let message;
+</script>
+
+<h1>
+    {status} - {message}
+</h1>
+
+<figure>
+    <img src={skalter} alt="Fallen Skalter">
+    <figcaption>Illustration by KuroTofu - <a href="https://twitter.com/Kuro_Tofu/status/1454254316710096899">source</a></figcaption>
+</figure>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+
+    figure {
+        text-align: center;
+    }
+</style>
---
+++ src/routes/__error.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+    export function load({ error, status }) {
+        return {
+            props: {
+                status,
+                message: error.message,
+            }
+        };
+    }
+  </script>
+  
+  <script>
+    import ErrorScreen from "$lib/components/ErrorScreen.svelte";
+    export let status;
+    export let message;
+  </script>
+  
+<ErrorScreen {status} {message} />
