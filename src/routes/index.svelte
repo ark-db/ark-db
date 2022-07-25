0851c873c5545c35c92387b0beb00a8ff5281f70
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-25 14:49:34 -0400

    add head metadata for pages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
+<svelte:head>
+  <title>ArkDB</title>
+  <meta name="description" content="Utility website for Arknights players" />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/planner.svelte
@@ -0,0 +1,4 @@
+<svelte:head>
+  <title>Upgrade Planner - ArkDB</title>
+  <meta name="description" content="Arknights operator upgrade cost calculator and planner" />
+</svelte:head>
