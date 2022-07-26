4eeee076d76910f0752e2642d47713ca06c2a6cb
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-26 16:11:58 -0400

    add bg to item icon based on rarity

--- src/lib/components/ItemIcon.svelte
+++ src/lib/components/ItemIcon.svelte
@@ -1,6 +1,20 @@
 <script>
+    import items from "../data/items.json";
     export let itemId;
-    let src = new URL(`../data/images/items/${itemId}.png`, import.meta.url).href
+
+    let itemSrc = new URL(`../data/images/items/${itemId}.png`, import.meta.url).href;
+    let name = items[itemId].name;
+    let bgSrc = new URL(`../data/images/bg/${items[itemId].rarity}.png`, import.meta.url).href;
 </script>
 
-<img {src} alt="item" />
+<img src={bgSrc} alt="background" />
+<img src={itemSrc} alt={name} />
+
+<style>
+    img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        transform: translate(-50%, -50%);
+    }
+</style>
