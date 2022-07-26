55d3935535ced7f90a7cf3f33c56d0142adee0b4
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-26 17:25:39 -0400

    import char data

---
+++ scripts/operators.py
@@ -0,0 +1,14 @@
+import requests
+
+chars = (
+    requests.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/character_table.json")
+            .json()
+)
+
+patch_chars = (
+    requests.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/char_patch_table.json")
+            .json()
+            ["patchChars"]
+)
+
+chars.update(patch_chars)
