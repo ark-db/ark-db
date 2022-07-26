41cae2bc6b066267974b25ad2321ebe364e63bb8
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-26 19:52:34 -0400

    get module data

--- scripts/operators.py
+++ scripts/operators.py
@@ -12,6 +12,11 @@ patch_chars = (
             ["patchChars"]
 )
 
+modules = (
+    requests.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/uniequip_table.json")
+            .json()
+)
+
 name_changes = {
     "char_118_yuki": "Shirayuki",
     "char_196_sunbr": "Gummy",
@@ -35,10 +40,12 @@ def format_cost(cost):
 
 for char_id, char_info in chars.items():
     if is_operator(char_info):
+        module_ids = modules["charEquip"].get(char_id, [])[1:]
         char_data[char_id] = {
             "charId": char_id,
             "name": name_changes.get(char_id, char_info["appellation"]),
             "elite": [format_cost(phase["evolveCost"]) for phase in char_info["phases"][1:]],
             "skill": [format_cost(level["lvlUpCost"]) for level in char_info["allSkillLvlup"]],
             "mastery": [[format_cost(mastery["levelUpCost"]) for mastery in skill["levelUpCostCond"]] for skill in char_info["skills"]],
+            "module": [[format_cost(cost) for cost in modules["equipDict"][module_id]["itemCost"].values()] for module_id in module_ids],
         }
