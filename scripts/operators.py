3776945f56804959de6e8627434e5a95adc89e30
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-26 19:08:54 -0400

    (1/?) override some op names + format elite costs

--- scripts/operators.py
+++ scripts/operators.py
@@ -12,6 +12,15 @@ patch_chars = (
             ["patchChars"]
 )
 
+name_changes = {
+    "char_118_yuki": "Shirayuki",
+    "char_196_sunbr": "Gummy",
+    "char_115_headbr": "Zima",
+    "char_195_glassb": "Istina",
+    "char_197_poca": "Rosa",
+    "char_1001_amiya2": "Amiya (Guard)"
+}
+
 chars.update(patch_chars)
 
 char_data = defaultdict(dict)
@@ -23,4 +32,8 @@ def is_operator(char_info):
 
 for char_id, char_info in chars.items():
     if is_operator(char_info):
-        pass
+        char_data[char_id] = {
+            "charId": char_id,
+            "name": name_changes.get(char_id, char_info["appellation"]),
+            "elite": [[{k: v for k, v in mat.items() if k != "type"} for mat in phase["evolveCost"]] for phase in char_info["phases"][1:]]
+        }
