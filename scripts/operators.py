6c6211eef1ac586d71e27a2878ca2a8e94a19afd
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-26 18:04:52 -0400

    filter out non-operators

--- scripts/operators.py
+++ scripts/operators.py
@@ -1,4 +1,5 @@
 import requests
+from collections import defaultdict
 
 chars = (
     requests.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/character_table.json")
@@ -12,3 +13,14 @@ patch_chars = (
 )
 
 chars.update(patch_chars)
+
+char_data = defaultdict(dict)
+
+def is_operator(char_info):
+    return char_info["profession"] != "TOKEN" \
+           and char_info["profession"] != "TRAP" \
+           and not char_info["isNotObtainable"]
+
+for char_id, char_info in chars.items():
+    if is_operator(char_info):
+        pass
