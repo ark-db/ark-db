9755680f38b27e90b5cf29d1999a8f43e657af93
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-27 12:41:11 -0400

    add border to operator based on rarity

--- src/lib/components/OperatorIcon.svelte
+++ src/lib/components/OperatorIcon.svelte
@@ -2,7 +2,26 @@
     import operators from "../data/operators.json";
     export let selectedChar;
     $: src = new URL(`../images/operators/${$selectedChar}.png`, import.meta.url).href;
-    let { name } = operators[$selectedChar];
+    $: ({ name, rarity } = operators[$selectedChar]);
 </script>
 
-<img {src} alt={name} />
+<img class="{rarity}" {src} alt={name} />
+
+<style>
+    img {
+        border-width: 5px;
+        border-style: solid;
+    }
+    img.\36 {
+        border-color: #ed702d;
+    }
+    img.\35 {
+        border-color: #f3b13e;
+    }
+    img.\34 {
+        border-color: #d4b3d8;
+    }
+    img.\33 {
+        border-color: #4faff0;
+    }
+</style>
