1f7520cd6447f0aba3d4d00565aef4c4e0ec04e6
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-26 22:48:40 -0400

    add operator search bar component

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Typeahead from "svelte-typeahead";
+    import operators from "../data/operators.json";
+
+    const data = Object.values(operators)
+    const extract = (id) => id.name;
+</script>
+  
+<Typeahead {data} {extract} />
