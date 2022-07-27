f3ec0c2a8e40d8faf4ac017b72f76179aeb8566a
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-27 14:26:06 -0400

    make import statements consistent

--- src/routes/planner.svelte
+++ src/routes/planner.svelte
@@ -6,7 +6,7 @@
 <script>
     import { writable } from 'svelte/store';
     import SearchBar from "$lib/components/SearchBar.svelte";
-    import OperatorIcon from "../lib/components/OperatorIcon.svelte";
+    import OperatorIcon from "$lib/components/OperatorIcon.svelte";
     const selectedChar = writable({charId: "", name: ""});
 </script>
 
