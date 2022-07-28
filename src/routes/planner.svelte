4f89e3660ac90365b25a9cea9a83ef4070ec1192
astral4 <88992929+astral4@users.noreply.github.com>
2022-07-27 20:17:01 -0400

    add elite rank checkboxes

--- src/routes/planner.svelte
+++ src/routes/planner.svelte
@@ -8,6 +8,7 @@
     import SearchBar from "$lib/components/SearchBar.svelte";
     import OperatorIcon from "$lib/components/OperatorIcon.svelte";
     const selectedChar = writable("");
+    $: ({ elite, skill, mastery, module } = $selectedChar);
 </script>
 
 <div class="select">
@@ -17,6 +18,12 @@
             <OperatorIcon {...$selectedChar} />
             <h1>{$selectedChar.name}</h1>
         </div>
+        {#each elite as cost, rank}
+            <div class="check elite">
+                <input type="checkbox">
+                <p>Elite {rank+1}</p>
+            </div>
+        {/each}
     {/if}
 </div>
 
@@ -33,5 +40,14 @@
     }
     .heading h1 {
         margin-left: 0.75em;
+    }
+    .check {
+        margin: 5px;
+        padding: 10px;
+        background-color: rgb(192, 192, 192);
+        display: flex;
+        align-items: center;
+        gap: 0.5em;
+        max-height: 10px;
     }
 </style>
