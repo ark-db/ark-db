8e90f7968b0420afac8b2ad37ee0ffa3d282abca
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-01 10:55:06 -0400

    taskitem v2

--- src/lib/components/TaskItem.svelte
+++ src/lib/components/TaskItem.svelte
@@ -1,13 +1,15 @@
 <script>
     import operators from "../data/operators.json";
     import OperatorIcon from "./OperatorIcon.svelte";
+    export let ready = false;
     export let charId;
     export let upgradeName;
 </script>
 
-<section>
+<section class={ready ? "ready" : "notready"}>
+    <input type="checkbox" bind:checked={ready}>
     <div class="icon">
-        <OperatorIcon {charId} --size="50px" />
+        <OperatorIcon {charId} --size="50px" --border="0px" />
     </div>
     <h2>{operators[charId].name}</h2>
     <p>{upgradeName}</p>
@@ -16,28 +18,39 @@
 <style>
     section {
         padding: 10px;
-        margin-top: 5px;
-        background-color: rgb(235, 238, 244);
         display: grid;
-        grid-template-columns: 50px 20em;
+        grid-template-columns: 1.25em 50px auto;
         grid-template-rows: 30px 30px;
-        column-gap: 1.25em;
+        column-gap: 1em;
         justify-items: start;
         align-items: center;
     }
-    .icon {
+    section.ready {
+        background-color: rgb(151, 255, 148);
+    }
+    section.notready {
+        background-color: rgb(255, 140, 140);
+    }
+
+    input[type=checkbox] {
         grid-column: 1 / 2;
         grid-row: 1 / 3;
+        transform: scale(1.5);
+        justify-self: center
+    }
+    .icon {
+        grid-column: 2 / 3;
+        grid-row: 1 / 3;
         display: flex;
+        justify-self: center;
         align-items: center;
     }
     h2 {
-        grid-column: 2 / 3;
+        grid-column: 3 / 4;
         grid-row: 1 / 2;
     }
     p {
-        grid-column: 2 / 3;
+        grid-column: 3 / 4;
         grid-row: 2 / 3;
     }
-
 </style>
