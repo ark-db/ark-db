031312ee171bdf41dc5c68bd27c7ed1935e6a74a
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-03 12:48:50 -0400

    css fixes

--- src/lib/components/ItemIcon.svelte
+++ src/lib/components/ItemIcon.svelte
@@ -5,7 +5,7 @@
 
     let itemSrc = new URL(`../images/items/${itemId}.png`, import.meta.url).href;
     let { name, rarity } = items[itemId];
-    let bgSrc = new URL(`../images/ranks/rarity/${rarity}.png`, import.meta.url).href;
+    let bgSrc = new URL(`../images/rarities/${rarity}.png`, import.meta.url).href;
 </script>
 
 <div>
@@ -25,12 +25,17 @@
     div img {
         grid-area: 1 / 1 / 2 / 2;
         place-self: center;
+        max-width: var(--size);
+        max-height: var(--size);
     }
     div p {
         grid-area: 1 / 1 / 2 / 2;
         place-self: end;
-        margin: 0 calc(var(--size) / 20) calc(var(--size) / 20) 0;
-        padding: 0.2em;
-        background-color: rgba(0, 0, 0, 0.7);
+        margin: 0 calc(var(--size) / 6.8) calc(var(--size) / 9.9) 0;
+        padding: 0.1em 0.3em 0.1em 0.3em;
+        background-color: rgba(0, 0, 0, 0.6);
+        box-shadow: 0.1em 0.2em 0.2em rgba(0, 0, 0, 0.45);
+        color: white;
+        font-weight: 300;
     }
 </style>
