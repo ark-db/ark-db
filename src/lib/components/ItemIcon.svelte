c5b69de8c3b9e48c8836e7d2e23673eb7cf70ec8
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-03 17:12:42 -0400

    ...

--- src/lib/components/ItemIcon.svelte
+++ src/lib/components/ItemIcon.svelte
@@ -37,7 +37,7 @@
         grid-area: 1 / 1 / 2 / 2;
         place-self: end;
         margin: 0 calc(var(--size) / 6.1) calc(var(--size) / 9.2) 0;
-        padding: 0.1em 0.3em 0.1em 0.3em;
+        padding: 0.1em 0.3em 0.05em 0.3em;
         background-color: rgba(0, 0, 0, 0.7);
         box-shadow: 0.1em 0.2em 0.2em rgba(0, 0, 0, 0.45);
         color: white;
