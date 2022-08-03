ea7253b9320b15972a3d35d213bf21890d3a72dc
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-03 11:38:27 -0400

    change to grid layout + add qty indicator

--- src/lib/components/ItemIcon.svelte
+++ src/lib/components/ItemIcon.svelte
@@ -1,20 +1,36 @@
 <script>
     import items from "../data/items.json";
     export let itemId;
+    export let qty = undefined;
 
     let itemSrc = new URL(`../images/items/${itemId}.png`, import.meta.url).href;
     let { name, rarity } = items[itemId];
     let bgSrc = new URL(`../images/ranks/rarity/${rarity}.png`, import.meta.url).href;
 </script>
 
-<img src={bgSrc} alt="background" />
-<img src={itemSrc} alt={name} />
+<div>
+    <img src={bgSrc} alt="background" />
+    <img src={itemSrc} alt={name} />
+    {#if qty !== undefined}
+        <p>{qty}</p>
+    {/if}
+</div>
 
 <style>
-    img {
-        position: absolute;
-        top: 0;
-        left: 0;
-        transform: translate(-50%, -50%);
+    div {
+        display: grid;
+        grid-template-columns: var(--size);
+        grid-template-rows: var(--size);
+    }
+    div img {
+        grid-area: 1 / 1 / 2 / 2;
+        place-self: center;
+    }
+    div p {
+        grid-area: 1 / 1 / 2 / 2;
+        place-self: end;
+        margin: 0 calc(var(--size) / 20) calc(var(--size) / 20) 0;
+        padding: 0.2em;
+        background-color: rgba(0, 0, 0, 0.7);
     }
 </style>
