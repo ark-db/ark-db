1c6a340300dfcaf68e5cbf965dc5c2ced3e6d409
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-04 10:42:27 -0400

    count item costs

---
+++ src/routes/planner/tally@planner.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { allSelected } from "../stores.js";
+
+    $: itemCounter = counter($allSelected.map(upgrade => upgrade.cost).flat());
+    $: console.log(itemCounter);
+
+    const counter = (list) => {
+        return Object.entries(list.reduce((prev, curr) => ({...prev, [curr.id]: curr.count + (prev[curr.id] ?? 0)}), {}))
+                     .map(item => ({id: item[0], count: item[1]}));
+    };
+</script>
