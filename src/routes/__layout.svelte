832c2c1661eb2c4d194b8f176b6807eb726d0d88
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-04 19:30:57 -0400

    consolidate appbar component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,94 @@
 <script>
-    import AppBar from "$lib/components/AppBar.svelte";
+    import { page } from '$app/stores';
+    const logo = "/favicon.svg"
 </script>
 
-<AppBar />
+
+
+<header>
+    <div class="logo">
+        <a href="/">
+			<img src={logo} alt="ArkDB">
+		</a>
+    </div>
+
+    <nav>
+        <ul>
+            <li class:active={$page.url.pathname === '/'}>
+                <a href="/">Home</a>
+            </li>
+			<li class:active={$page.url.pathname.startsWith("/planner")}>
+				<a href="/planner">Upgrade Planner</a>
+			</li>
+        </ul>
+    </nav>
+</header>
 
 <main>
     <slot />
 </main>
+
+
+
+<style>
+	header {
+		display: flex;
+        background: rgb(190, 229, 249);
+	}
+
+	.logo {
+		width: 3em;
+		height: 3em;
+	}
+
+	.logo a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.logo img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+        border-bottom: 3px solid transparent;
+	}
+
+    li.active {
+        border-bottom: 3px solid skyblue;
+        box-sizing: border-box;
+    }
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1em;
+		color: black;
+		text-decoration: none;
+	}
+</style>
