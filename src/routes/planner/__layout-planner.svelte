90f46c6add696772fc13cdcda8409b15a2083096
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-03 21:33:01 -0400

    add tab switching for planner dir

--- src/routes/planner/__layout-planner.svelte
+++ src/routes/planner/__layout-planner.svelte
@@ -1,9 +1,46 @@
 <script>
-    import AppBar from "$lib/components/AppBar.svelte";
+    import { page } from '$app/stores';
 </script>
 
-<AppBar />
+
+
+<header>
+    <nav>
+        <a href="/planner" class:active={$page.url.pathname === "/planner"}>Goal Tracker</a>
+        <a href="/planner/tally" class:active={$page.url.pathname === "/planner/tally"}>Cost Tracker</a>
+    </nav>
+</header>
 
 <main>
     <slot />
 </main>
+
+
+
+<style>
+    header {
+        margin-top: 10px;
+    }
+    nav {
+        display: flex;
+        flex-flow: row wrap;
+        align-items: center;
+        justify-content: space-evenly;
+        gap: 0.5em;
+    }
+    a {
+        flex-grow: 1;
+        display: flex;
+        justify-content: center;
+        padding: 0.5em 1em 0.5em 1em;
+        background-color: rgb(235, 245, 253);
+        border-bottom: 2px solid transparent;
+        box-sizing: border-box;
+        max-width: 15em;
+        text-decoration: none;
+        color: initial;
+    }
+    a.active {
+        border-bottom: 2px solid rgb(109, 112, 114);
+    }
+</style>
