4ea954d72875fea1d62b45dbf8b0c7b32f17d3d2
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-04 10:55:54 -0400

    sort items by sortId

--- src/routes/planner/tally@planner.svelte
+++ src/routes/planner/tally@planner.svelte
@@ -1,11 +1,13 @@
 <script>
     import { allSelected } from "../stores.js";
+    import items from "/src/lib/data/items.json?import"
 
     $: itemCounter = counter($allSelected.map(upgrade => upgrade.cost).flat());
     $: console.log(itemCounter);
 
     const counter = (list) => {
         return Object.entries(list.reduce((prev, curr) => ({...prev, [curr.id]: curr.count + (prev[curr.id] ?? 0)}), {}))
-                     .map(item => ({id: item[0], count: item[1]}));
+                     .map(item => ({id: item[0], count: item[1]}))
+                     .sort((prev, curr) => items[prev.id].sortId - items[curr.id].sortId);
     };
 </script>
