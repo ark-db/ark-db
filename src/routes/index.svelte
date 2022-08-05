6dcc5fc7a5ccede9cc0b552b724edf6938ebb228
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-05 15:53:26 -0400

    feedback link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,7 @@
 <h1>ArkDB</h1>
 <h2>A utility website for Arknights players</h2>
 <p>v0.1.0-beta</p>
-
+<a href="https://form.typeform.com/to/gSMV12RK">Submit feedback</a>
 
 <style>
     h1 {
@@ -19,6 +19,12 @@
         font-size: clamp(1em, 5vw, 2em);
     }
     p {
+        text-align: center;
+    }
+    a {
+        margin-top: 5em;
+        color: black;
+        display: block;
         text-align: center;
     }
 </style>
