cfc72d439e418ce0d28ea920c7d823c4d15b0a12
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-08 19:08:09 -0400

    refactor flipDurationMs into @const

--- src/routes/planner/index@planner.svelte
+++ src/routes/planner/index@planner.svelte
@@ -17,7 +17,6 @@
     $: uid = $allSelected.length;
     $: allReady = $allSelected.filter(upgrade => upgrade.ready);
     $: allNotReady = $allSelected.filter(upgrade => !upgrade.ready);
-    const flipDurationMs = 150;
 
     $: selectedUpgradeNames = writable(new Array($selectedChar?.upgrades?.length).fill(new Set()))
 
@@ -98,6 +97,7 @@
 
 <h1>Upgrades</h1>
 {#if $allSelected.length > 0}
+    {@const flipDurationMs = 150}
     <section id="taskboard">
     {#if $splitByStatus}
         {#if allNotReady.length > 0}
