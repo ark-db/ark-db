c5953fa7e26dd2d75ec3bd550709814db8267d6f
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-09 09:41:21 -0400

    loop optimizations

--- scripts/operators.py
+++ scripts/operators.py
@@ -6,12 +6,9 @@ import json
 chars = (
     requests.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/character_table.json")
             .json()
-)
-
-patch_chars = (
-    requests.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/char_patch_table.json")
-            .json()
-            ["patchChars"]
+    | requests.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/char_patch_table.json")
+              .json()
+              ["patchChars"]
 )
 
 skills = (
@@ -39,11 +36,8 @@ name_changes = {
     "char_1001_amiya2": "Amiya (Guard)"
 }
 
-chars.update(patch_chars)
-
 char_data = defaultdict(dict)
 skill_ids = set()
-bad_ids = []
 
 def is_operator(char_info):
     return char_info["profession"] != "TOKEN" \
@@ -72,8 +66,11 @@ for char_id, char_info in chars.items():
         }
 
         for i, skill in enumerate(char_info["skills"]):
-            if char_info["rarity"] > 2:
-                skill_ids.add(get_skill_id(skill))
+            if char_info["rarity"] > 2 and (skill_id := get_skill_id(skill)) not in skill_ids:
+                skill_ids.add(skill_id)
+                icon_url = f"https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/img/skills/skill_icon_{skill_id}.png"
+                utils.save_image(icon_url, "skills", skill_id)
+
             char_data[char_id]["upgrades"].append(
                 {"cls": f"mastery",
                  "skillId": get_skill_id(skill),
@@ -84,6 +81,9 @@ for char_id, char_info in chars.items():
 
         module_ids = modules["charEquip"].get(char_id, [])[1:]
         for i, module_id in enumerate(module_ids):
+            icon_url = f"https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/img/equip/icon/{module_id}.png"
+            utils.save_image(icon_url, "modules", module_id)
+
             module_info = modules['equipDict'][module_id]
             char_data[char_id]["upgrades"].append(
                 {"cls": f"module",
@@ -95,14 +95,6 @@ for char_id, char_info in chars.items():
 
         icon_url = f"https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/img/avatars/{char_id}.png"
         utils.save_image(icon_url, "operators", char_id)
-
-        for module_id in module_ids:
-            icon_url = f"https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/img/equip/icon/{module_id}.png"
-            utils.save_image(icon_url, "modules", module_id)
-
-for skill_id in skill_ids:
-    icon_url = f"https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/img/skills/skill_icon_{skill_id}.png"
-    utils.save_image(icon_url, "skills", skill_id)
 
 with open("./src/lib/data/operators.json", "w") as f:
     json.dump(char_data, f)
