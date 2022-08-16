e3cf2b97cfd4f0edf3b3569278d02bb480c3843e
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-16 17:22:23 -0400

    remove unnecessary drop

--- scripts/msv.py
+++ scripts/msv.py
@@ -36,7 +36,6 @@ def get_drop_data(region: Region) -> pd.DataFrame:
                   and itemId in @ALLOWED_ITEMS")
           .pipe(trim_stage_ids)
           .assign(drop_rate = lambda df: df["quantity"] / df["times"])
-          .drop(["quantity", "times"], axis=1)
           .pivot(index="stageId",
                  columns="itemId",
                  values="drop_rate")
