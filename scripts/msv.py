23d812fdad1ae8a570620433d14cefc1e0f8dd1f
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-16 14:26:30 -0400

    refactor for clarity

--- scripts/msv.py
+++ scripts/msv.py
@@ -10,17 +10,17 @@ class Region(Enum):
 MIN_RUN_THRESHOLD = 100
 
 def get_drop_data(region: Region) -> pd.DataFrame:
-    current_stages = set(map(
-        lambda entry: entry["stageId"],
+    current_stages = (
         requests.get(f"https://penguin-stats.io/PenguinStats/api/v2/result/matrix?server={region.value}")
                 .json()
                 ["matrix"]
-    ))
+    )
+    current_stage_ids = set(stage["stageId"] for stage in current_stages)
     stages = (
         pd.DataFrame(requests.get("https://penguin-stats.io/PenguinStats/api/v2/result/matrix?show_closed_zones=true")
                              .json()
                              ["matrix"])
-          .query("stageId in @current_stages \
+          .query("stageId in @current_stage_ids \
                   and times >= @MIN_RUN_THRESHOLD")
     )
     print(stages.head())
