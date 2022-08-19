7a303f624e914e9816834273aec3014798130a7e
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-19 11:33:08 -0400

    add svelte:head

--- src/routes/farming/index.svelte
+++ src/routes/farming/index.svelte
@@ -1,3 +1,11 @@
+<svelte:head>
+    <title>Farming Stages</title>
+    <meta name="description"
+          content="A list of the best Arknights farming stages for materials,
+                   with stage efficiencies, drop rates, and expected sanity costs."
+    />
+</svelte:head>
+
 <script>
     import farming from "$lib/data/farming.json";
     import items from "$lib/data/items.json";
