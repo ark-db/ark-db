ebee67ac365b0aad91261964c08d7c0a8fd3c0ca
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-19 11:39:52 -0400

    create "region" store + reorganize stores.js

--- src/routes/farming/index.svelte
+++ src/routes/farming/index.svelte
@@ -9,6 +9,7 @@
 <script>
     import farming from "$lib/data/farming.json";
     import items from "$lib/data/items.json";
+    import { region } from "../stores.js";
 </script>
 
 <p>Lorem ipsum</p>
--- src/routes/stores.js
+++ src/routes/stores.js
@@ -1,9 +1,17 @@
 import { writable } from "svelte/store";
 import items from "$lib/data/items.json";
 
+// planner
 export const selectedChar = writable();
 export const activeCategory = writable();
 export const allSelected = writable([]);
+
+export const splitByStatus = writable(false);
+export const showCost = writable(false);
+
+
+
+// planner/cost
 export const inventory = writable(
     Object.keys(items)
           .sort((prev, curr) => items[prev].sortId - items[curr].sortId)
@@ -11,8 +19,11 @@ export const inventory = writable(
           .map(id => ({id, count: 0}))
 );
 
-export const splitByStatus = writable(false);
-export const showCost = writable(false);
 export const costFilter = writable([false]);
 export const itemFilter = writable(["material", "skill", "module", "chip", "misc"]);
 export const makeT3 = writable(false);
+
+
+
+// farming
+export const region = writable("global");
