3a9dcdae5d9aece3acb8010c9f943409b35de381
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-26 11:01:31 -0400

    data structure overhaul

--- scripts/operators.py
+++ scripts/operators.py
@@ -57,54 +57,81 @@ for char_id, char_info in chars.items():
             "charId": char_id,
             "name": name_changes.get(char_id, char_info["appellation"]),
             "rarity": char_info["rarity"] + 1,
-            "upgrades": [
-                {"data": [
-                    {"name": f"Elite {i+1}",
-                     "cost": utils.format_cost(phase["evolveCost"])
-                             + [{"id": "4001", "count": elite_lmd_costs[char_info["rarity"]][i]}]}
-                    for i, phase in enumerate(char_info["phases"][1:])
-                ]},
-                {"data": [
-                    {"name": f"Skill Level {i+2}",
-                     "cost": utils.format_cost(level["lvlUpCost"])}
-                    for i, level in enumerate(char_info["allSkillLvlup"])
-                ]}
-            ]
+            "upgrades": [],
+            "costs": dict()
         }
 
-        for i, skill in enumerate(char_info["skills"]):
-            skill_id = get_skill_id(skill)
+        upgrade_names = []
+        for i, phase in enumerate(char_info["phases"][1:]):
+            name = f"Elite {i+1}"
+            upgrade_names.append(name)
+            char_data["costs"].update({
+                name: utils.format_cost(phase["evolveCost"])
+                      + [{"id": "4001", "count": elite_lmd_costs[char_info["rarity"]][i]}]
+            })
+        char_data["upgrades"].append({
+            "names": upgrade_names
+        })
 
+
+
+        upgrade_names = []
+        for i, level in enumerate(char_info["allSkillLvlup"]):
+            name = f"Skill Level {i+2}"
+            upgrade_names.append(name)
+            char_data["costs"].update({
+                name: utils.format_cost(level["lvlUpCost"])
+            })
+        char_data["upgrades"].append({
+            "names": upgrade_names
+        })
+
+
+
+        for i, skill in enumerate(char_info["skills"]):
+            upgrade_names = []
+
+            skill_id = get_skill_id(skill)
             if skill_id not in skill_ids and char_info["rarity"] > 2:
                 skill_ids.add(skill_id)
                 icon_url = f"https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/img/skills/skill_icon_{skill_id}.png"
                 utils.save_image(icon_url, "skills", skill_id)
 
+            for j, mastery in enumerate(skill["levelUpCostCond"]):
+                name = f"Skill {i+1} Mastery {j+1}"
+                upgrade_names.append(name)
+                char_data["costs"].update({
+                    name: utils.format_cost(mastery["levelUpCost"])
+                })
             char_data["upgrades"].append({
                 "cls": "mastery",
                 "skillId": skill_id,
-                "data": [
-                    {"name": f"Skill {i+1} Mastery {j+1}",
-                     "cost": utils.format_cost(mastery["levelUpCost"])}
-                    for j, mastery in enumerate(skill["levelUpCostCond"])
-                ]
+                "names": upgrade_names
             })
 
+
+
         module_ids = modules["charEquip"].get(char_id, [])[1:]
-        for i, module_id in enumerate(module_ids):
+        for module_id in module_ids:
+            upgrade_names = []
+
             icon_url = f"https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/img/equip/icon/{module_id}.png"
             utils.save_image(icon_url, "modules", module_id)
 
             module_info = modules["equipDict"][module_id]
+            for i, cost in enumerate(module_info["itemCost"].values()):
+                name = f"{module_info['typeIcon'].upper()} Stage {i+1}"
+                upgrade_names.append(name)
+                char_data["costs"].update({
+                    name: utils.format_cost(cost)
+                })
             char_data["upgrades"].append({
                 "cls": "module",
                 "moduleId": module_info["uniEquipId"],
-                "data": [
-                    {"name": f"{module_info['typeIcon'].upper()} Stage {i+1}",
-                     "cost": utils.format_cost(cost)}
-                    for i, cost in enumerate(module_info["itemCost"].values())
-                ]
+                "names": upgrade_names
             })
+
+
 
         icon_url = f"https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master/img/avatars/{char_id}.png"
         utils.save_image(icon_url, "operators", char_id)
