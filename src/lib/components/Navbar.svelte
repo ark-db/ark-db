c95b21628375b067c3d281427539ac88f55ca6dc
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-26 20:19:49 -0400

    refactor navbar into separate component

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,53 @@
+<script>
+    export let page;
+    export let logo;
+</script>
+
+<header>
+    <a href="/">
+		<img src={logo} alt="ArkDB">
+	</a>
+
+    <nav>
+        <a href="/" class:active={$page.url.pathname === "/"}>Home</a>
+		<a href="/planner" class:active={$page.url.pathname.startsWith("/planner")}>Planner</a>
+		<a href="/farming" class:active={$page.url.pathname === "/farming"}>Farming</a>
+    </nav>
+</header>
+
+<style>
+    header {
+		background-color: var(--dark-moderate);
+		box-shadow: 0.1em 0.1em 0.5em rgba(0, 0, 0, 0.75);
+		display: flex;
+		align-items: center;
+	}
+	img {
+		margin-left: 1em;
+		width: 100%;
+		max-width: 40px;
+		min-width: 40px;
+	}
+	nav {
+		padding: 1em;
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+		gap: 1em;
+	}
+	a {
+		border-radius: 6px;
+		padding: 0.5em 1em;
+		font-weight: 500;
+		text-decoration: none;
+		color: var(--med-strong);
+	}
+	nav a:hover:not(.active) {
+		background-color: var(--dark-mild);
+		color: white;
+	}
+	a.active {
+		background-color: var(--dark-strong);
+		color: white;
+	}
+</style>
