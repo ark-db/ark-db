aec375e7132e248a9289327c3f0eb24df87ad733
astral4 <88992929+astral4@users.noreply.github.com>
2022-08-27 14:13:21 -0400

    navbar pt1

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,10 +1,13 @@
 <script>
+    import { fly } from "svelte/transition";
     import bars from "../images/bars.svg";
     import leftArrow from "../images/left-arrow.svg";
 
     export let page;
 
     let active = false;
+
+    const toggle = () => active = !active;
 
     function getPageTitle(path) {
         if (path === "/") {
@@ -20,20 +23,43 @@
 
 
 {#if active}
-    {{}}
-{:else}
-    <header>
-        <input type="image" src={bars} alt="Open navigation menu" on:click={() => active = true}>
-        <!--<div class="logo">
+    <input type="image" class="close" src={leftArrow} alt="Close navigation menu" on:click={toggle} transition:fly={{x: 50, duration: 300}}>
+    <div class="sidebar" transition:fly={{x: -100, duration: 300}}>
+        <div class="logo">
             <slot name="logo" />
-        </div>-->
-        <h1>{getPageTitle($page.url.pathname)}</h1>
-    </header>
+        </div>
+        <nav>
+
+        </nav>
+    </div>
 {/if}
+
+<header>
+    <input type="image" class="open" src={bars} alt="Open navigation menu" on:click={toggle}>
+    <h1>{getPageTitle($page.url.pathname)}</h1>
+</header>
 
 
 
 <style>
+    input[type=image] {
+        position: fixed;
+        width: 100%;
+    }
+    input[type=image].close {
+        top: 0.5em;
+        left: 80%;
+        max-width: 1.5em;
+        min-width: 1.5em;
+        z-index: 3;
+    }
+    .sidebar {
+        position: fixed;
+        width: 75%;
+        z-index: 3;
+        background-color: var(--dark-moderate);
+        box-shadow: 0.1em 0.1em 0.5em rgba(0, 0, 0, 0.75);
+    }
     header {
         padding: 0.25em;
         position: fixed;
@@ -46,20 +72,16 @@
         justify-content: center;
         gap: 0.75em;
     }
-    input[type=image] {
-        position: fixed;
+    input[type=image].open {
         left: 1em;
-        width: 100%;
         max-width: 2em;
         min-width: 2em;
     }
-    /**
     .logo {
         width: 100%;
-        max-width: 2em;
-        min-width: 2em;
+        max-width: 4em;
+        min-width: 4em;
     }
-    */
     h1 {
         margin: 0.25em 0;
         font-weight: 500;
