27aa7e99ce99fc479b29f6bca805ca3cbfcbc653
astral4 <88992929+astral4@users.noreply.github.com>
2022-09-01 19:23:39 -0400

    reorder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
+<script>
+    import ghLogo from "$lib/images/github-logo.svg";
+</script>
+
+
+
 <svelte:head>
     <title>ArkDB - Arknights Tools</title>
     <meta name="description" content="Utility website for Arknights players" />
 </svelte:head>
-
-<script>
-    import ghLogo from "$lib/images/github-logo.svg";
-</script>
 
 <h1 class="title">ArkDB</h1>
 <h2>A utility website for Arknights players</h2>
@@ -13,6 +15,8 @@
 <a href="https://github.com/ark-db/ark-db/" target="_blank" rel="noreferrer">
     <img src={ghLogo} alt="GitHub logo">
 </a>
+
+
 
 <style>
     h2, p, a {
