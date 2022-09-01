4077c6078aa27cfda7819e1547890867b1062f20
astral4 <88992929+astral4@users.noreply.github.com>
2022-09-01 17:28:49 -0400

    save upgrades and inventory to localstorage

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -1,10 +1,30 @@
 import { writable } from "svelte/store";
+import { browser } from "$app/environment";
 import items from "$lib/data/items.json";
+
+let storedUpgrades;
+let storedInventory;
+
+if (browser) {
+    storedUpgrades = JSON.parse(localStorage.getItem("upgrades"));
+    storedInventory = JSON.parse(localStorage.getItem("inventory"));
+}
+
+
 
 // planner
 export const selectedChar = writable();
 export const activeCategory = writable();
-export const allSelected = writable([])
+export const allSelected = writable(storedUpgrades ?? []);
+export function updateStoredUpgrades(upgrades) {
+    if (browser) {
+        let reindexed = structuredClone(upgrades);
+        for (let [idx, upgrade] of reindexed.entries()) {
+            upgrade.id = idx;
+        }
+        localStorage.upgrades = JSON.stringify(reindexed);
+    }
+}
 
 export const splitByStatus = writable(false);
 export const showCost = writable(false);
@@ -12,12 +32,17 @@ export const showCost = writable(false);
 
 
 // planner/cost
-export const inventory = writable(
-    Object.keys(items)
-          .sort((prev, curr) => items[prev].sortId - items[curr].sortId)
-          .filter(id => items[id].type !== "misc")
-          .map(id => ({id, count: 0}))
-);
+const defaultInventory = Object.keys(items)
+                               .sort((prev, curr) => items[prev].sortId - items[curr].sortId)
+                               .filter(id => items[id].type !== "misc")
+                               .map(id => ({id, count: 0}));
+
+export const inventory = writable(storedInventory ?? defaultInventory);
+if (browser) {
+    inventory.subscribe(value => {
+        localStorage.inventory = JSON.stringify(value);
+    });
+}
 
 export const costFilter = writable([false]);
 export const itemFilter = writable(["material", "skill", "module", "chip", "misc"]);
