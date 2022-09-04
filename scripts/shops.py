2a4cb4bf49387b71f9c3c7ec1c7a9174d766cc66
astral4 <88992929+astral4@users.noreply.github.com>
2022-09-04 19:43:30 -0400

    get latest CC and SS (CN)

---
+++ scripts/shops.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def convert_to_utc(df: pd.DataFrame):
+    df["活动开始时间"] -= pd.Timedelta(hours=8)
+    df["活动开始时间"] = df["活动开始时间"].dt.tz_localize("UTC")
+    return df
+
+def get_name_of_latest(df: pd.DataFrame) -> str:
+    name, _, _ = (
+        df.iloc[0]
+          ["活动页面"]
+          .partition("(")
+    )
+    return name
+
+events = (
+    pd.read_html("https://prts.wiki/w/%E6%B4%BB%E5%8A%A8%E4%B8%80%E8%A7%88",
+                 parse_dates=["活动开始时间"])
+      [0]
+      .pipe(convert_to_utc)
+      .pipe(lambda df: df[df["活动开始时间"] < pd.Timestamp.utcnow()])
+)
+
+latest_cc = get_name_of_latest(
+    events.pipe(lambda df: df[df["活动分类"] == "危机合约"])
+)
+
+latest_ss = get_name_of_latest(
+    events.pipe(lambda df: df[df["活动分类"].isin({"支线故事", "故事集"})])
+)
+
+print(latest_cc, latest_ss)
