c9d5ce73bf93a275ecb9c34f01f770799f837f58
astral4 <88992929+astral4@users.noreply.github.com>
2022-09-06 16:14:09 -0400

    change output path

--- scripts/shops.py
+++ scripts/shops.py
@@ -114,7 +114,7 @@ all_shop_effics = {
 
 
  
-with open("./scripts/msv.json", "r") as f1, open("./scripts/shops.json", "w") as f2:
+with open("./scripts/msv.json", "r") as f1, open("./src/lib/data/shops.json", "w") as f2:
     sanity_values = json.load(f1)
 
     for ss in ss_events.itertuples():
