1f041b2bea684d5ecf54edcd1d0688bf447302ce
astral4 <88992929+astral4@users.noreply.github.com>
2022-09-06 20:31:31 -0400

    add region select to navbar + site name

---
+++ src/lib/components/RegionSelect.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { region } from "@stores";
+</script>
+
+
+
+<select bind:value={$region}>
+    <option value="glb">US/JP/KR</option>
+    <option value="cn">CN</option>
+</select>
+
+
+
+<style>
+    select {
+        border: 1px solid white;
+        border-radius: 0.75em;
+        padding: 0.75em 1em;
+        background-color: transparent;
+        width: 100%;
+        max-width: 10em;
+        color: var(--light-moderate);
+        font-weight: 600;
+    }
+</style>
