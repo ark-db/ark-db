55ff6ee1cbd6dbee4a3df9edd840a3fb61621ba4
astral4 <88992929+astral4@users.noreply.github.com>
2022-09-06 20:31:31 -0400

    add region select to navbar + site name

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,13 +1,17 @@
 <script>
     import { page } from "$app/stores";
 	import { links } from "@utils";
+	import RegionSelect from "./RegionSelect.svelte";
 </script>
 
 
 
 <header>
     <a href="/">
-		<slot name="logo" />
+		<div class="logo">
+			<slot name="logo" />
+		</div>
+		<h1>ArkDB</h1>
 	</a>
     <nav>
 		{#each [...links.entries()] as [ href, title ]}
@@ -16,25 +20,32 @@
 			</a>
 		{/each}
     </nav>
+	<div class="region-select">
+		<RegionSelect />
+	</div>
 </header>
 
 
 
 <style>
     header {
-        position: fixed;
-        width: 100%;
-        z-index: 2;
 		background-color: var(--dark-moderate);
 		box-shadow: 0.1em 0.1em 0.5em rgba(0, 0, 0, 0.75);
+		position: fixed;
+        width: 100%;
+        z-index: 2;
 		display: flex;
 		align-items: center;
 	}
-	header > a {
-		margin-left: 1em;
+	.logo {
 		width: 100%;
 		max-width: 40px;
 		min-width: 40px;
+	}
+	a > h1 {
+		margin: 0;
+		color: var(--light-moderate);
+		font-size: 1.5em;
 	}
 	nav {
 		padding: 1em;
@@ -49,6 +60,9 @@
 		font-weight: 500;
 		text-decoration: none;
 		color: var(--med-strong);
+		display: flex;
+		align-items: center;
+		gap: 0.75em;
 	}
 	nav a:hover:not(.active) {
 		background-color: var(--dark-mild);
@@ -57,5 +71,10 @@
 	a.active {
 		background-color: var(--dark-strong);
 		color: white;
+	}
+	.region-select {
+		position: fixed;
+		top: 1em;
+		right: 1em;
 	}
 </style>
