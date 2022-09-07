0392d186d9c59572fee00c8d68f414173c634e1c
astral4 <88992929+astral4@users.noreply.github.com>
2022-09-06 20:59:10 -0400

    add more styling with custom icons

--- src/lib/components/RegionSelect.svelte
+++ src/lib/components/RegionSelect.svelte
@@ -1,25 +1,44 @@
 <script>
     import { region } from "@stores";
+    import globe from "../images/globe.svg";
 </script>
 
 
 
-<select bind:value={$region}>
-    <option value="glb">US/JP/KR</option>
-    <option value="cn">CN</option>
-</select>
+<div>
+    <img src={globe} alt="Globe">
+    <select bind:value={$region}>
+        <option value="glb">US/JP/KR</option>
+        <option value="cn">CN</option>
+    </select>
+</div>
 
 
 
 <style>
+    div {
+        display: flex;
+        gap: 15px;
+    }
     select {
         border: 1px solid white;
         border-radius: 0.75em;
-        padding: 0.75em 1em;
+        padding: 0.75em 2em 0.75em 0.75em;
         background-color: transparent;
         width: 100%;
         max-width: 10em;
         color: var(--light-moderate);
         font-weight: 600;
+        appearance: none;
+        background-image: url("../images/arrow-down.svg");
+        background-repeat: no-repeat;
+        background-position-x: calc(100% - 10px);
+        background-position-y: 50%;
+        background-size: 10px;
+    }
+    img {
+        width: 100%;
+        max-width: 20px;
+        min-width: 20px;
     }
 </style>
