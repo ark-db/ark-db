030f2c2fbe2853be83ba374cc42113a6d16851f1
astral4 <88992929+astral4@users.noreply.github.com>
2022-09-08 13:53:12 -0400

    refactor item grid into separate component

---
+++ src/lib/components/ShopItems.svelte
@@ -0,0 +1,76 @@
+<script>
+    import ItemIcon from "./ItemIcon.svelte";
+    export let items, normalizeValues;
+
+    const maxValue = Math.max(...items.map(item => item.value));
+
+    function categorize(score) {
+        if (score < 0.5) return "poor";
+        else if (score < 0.7) return "bad";
+        else if (score < 0.8) return "fair"
+        else if (score < 0.9) return "good";
+        else return "great";
+    };
+</script>
+
+
+
+<div class="items">
+    {#each items as { id, count, stock, value }}
+        {@const effic = value / maxValue}
+        <div class="item">
+            <ItemIcon {id} {count} --size="75px"/>
+            {#if stock !== -2}
+                <p class="stock" title="Item stock">
+                    {stock !== -1 ? stock : "∞"}
+                </p>
+            {/if}
+            {#if value !== -1}
+                <p class="effic {categorize(effic)}"
+                   title={$normalizeValues ? "Relative efficiency" : "Sanity value per token"}
+                >
+                    {$normalizeValues ? effic.toFixed(3) : value}
+                </p>
+            {:else}
+                <p class="effic none"
+                   title={$normalizeValues ? "Relative efficiency" : "Sanity value per token"}
+                >
+                    ——
+                </p>
+            {/if}
+        </div>
+    {/each}
+</div>
+
+
+
+<style>
+    .items {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, calc(75px + 6em));
+        justify-content: center;
+        gap: 20px;
+    }
+    .item {
+        position: relative;
+        display: flex;
+        align-items: center;
+        gap: 2.75em;
+    }
+    .effic {
+        font-weight: 600;
+    }
+    .effic.none {
+        color: var(--light-moderate);
+    }
+    .stock {
+        position: absolute;
+        top: -5px;
+        left: 30px;
+        z-index: -1;
+        padding: 0.15em 0.5em 0.15em 3em;
+        border-radius: 1em;
+        background-color: var(--light-moderate);
+        font-weight: 500;
+    }
+</style>
--- src/routes/shops/+page.svelte
+++ src/routes/shops/+page.svelte
@@ -2,15 +2,7 @@
     import { assets } from "$app/paths";
     import { region, normalizeValues } from "@stores";
     import shops from "$lib/data/event_shops.json";
-    import ItemIcon from "$lib/components/ItemIcon.svelte";
-
-    function categorize(score) {
-        if (score < 0.5) return "poor";
-        else if (score < 0.7) return "bad";
-        else if (score < 0.8) return "fair"
-        else if (score < 0.9) return "good";
-        else return "great";
-    };
+    import ShopItems from "$lib/components/ShopItems.svelte";
 </script>
 
 
@@ -32,36 +24,12 @@
     {@const shopItems = shops.shops[$region]?.[type]}
     {#if eventName}
         {@const src = `${assets}/images/events/${$region}_${type}_banner.webp`}
-        {@const maxValue = Math.max(...shopItems.map(item => item.value))}
         <section class="event">
             <div class="top">
                 <h1 class="title">{eventName}</h1>
                 <img class="banner" {src} alt={eventName}>
             </div>
-            <div class="items">
-                {#each shopItems as { id, count, stock, value }}
-                    {@const effic = value / maxValue}
-                    <div class="item">
-                        <ItemIcon {id} {count} --size="75px"/>
-                        <p class="stock" title="Item stock">
-                            {stock !== -1 ? stock : "∞"}
-                        </p>
-                        {#if value !== -1}
-                            <p class="effic {categorize(effic)}"
-                               title={$normalizeValues ? "Relative efficiency" : "Sanity value per token"}
-                            >
-                                {$normalizeValues ? effic.toFixed(3) : value}
-                            </p>
-                        {:else}
-                            <p class="effic none"
-                               title={$normalizeValues ? "Relative efficiency" : "Sanity value per token"}
-                            >
-                                ——
-                            </p>
-                        {/if}
-                    </div>
-                {/each}
-            </div>
+            <ShopItems items={shopItems} {normalizeValues}/>
         </section>
     {/if}
 {/each}
@@ -97,33 +65,5 @@
     }
     .banner {
         width: clamp(1px, 80vw, 600px);
-    }
-    .items {
-        display: grid;
-        grid-template-columns: repeat(auto-fill, calc(75px + 6em));
-        justify-content: center;
-        gap: 20px;
-    }
-    .item {
-        position: relative;
-        display: flex;
-        align-items: center;
-        gap: 2.75em;
-    }
-    .effic {
-        font-weight: 600;
-    }
-    .effic.none {
-        color: var(--light-moderate);
-    }
-    .stock {
-        position: absolute;
-        top: -5px;
-        left: 30px;
-        z-index: -1;
-        padding: 0.15em 0.5em 0.15em 3em;
-        border-radius: 1em;
-        background-color: var(--light-moderate);
-        font-weight: 500;
     }
 </style>
