ff8271c2923c3f9494fa5eacc58fa0e56021d337
astral4 <88992929+astral4@users.noreply.github.com>
2022-09-11 17:05:01 -0400

    add categories param to API

--- src/routes/api/operators/+server.js
+++ src/routes/api/operators/+server.js
@@ -3,9 +3,29 @@ import operators from "$lib/data/operators.json";
 
 export function GET({ url }) {
     const charId = url.searchParams.get("id");
-    const charData = operators?.[charId];
-    if (charData) return json({
-        data: charData
-    });
-    throw error(400, "Invalid character ID");
+    const categories = [url.searchParams.get("categories")].flat();
+    if (charId) {
+        const charData = operators?.[charId]
+        if (charData) {
+            if (categories.length > 0) {
+                return json(
+                    Object.fromEntries(
+                        Object.entries(charData)
+                              .filter(([k, v]) => categories.includes(k))
+                    )
+                )
+            }
+            return json(charData);
+        }
+        throw error(400, "Invalid character ID");
+    }
+    if (categories.length > 0) {
+        return json(
+            Object.fromEntries(
+                Object.entries(operators)
+                      .map(([id, data]) => ([id, Object.fromEntries(Object.entries(data)
+                                                                          .filter(([k, v]) => categories.includes(k)))])))
+        )
+    }
+    return json(operators);
 };
