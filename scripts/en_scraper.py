bed57b1a6d85bb11686633b4c945c2425f0570b2
astral4 <88992929+astral4@users.noreply.github.com>
2022-10-07 15:30:22 -0400

    .

--- scripts/en_scraper.py
+++ scripts/en_scraper.py
@@ -16,7 +16,7 @@ options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 
 driver = webdriver.Firefox(
-    service=Service(GeckoDriverManager(path="./").install()),
+    service=Service(GeckoDriverManager().install()),
     options=options
 )
 
