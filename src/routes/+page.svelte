aff3df8e892ca55c59c315bd050f6f182543d39a
astral4 <88992929+astral4@users.noreply.github.com>
2022-10-07 00:38:43 -0400

    increment version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 
 <h1 class="title">ArkDB</h1>
 <h2>A utility website for Arknights players</h2>
-<p>v0.13.1</p>
+<p>v0.14.0</p>
 <a href="https://github.com/ark-db/ark-db/" target="_blank" rel="noreferrer">
     <img src={ghLogo} alt="GitHub logo">
 </a>
